4912e1d38c4a7991e6d91e50d0d35b475481aafc
Toropigeon <71664787+TheAnton1@users.noreply.github.com>
2024-03-05 15:58:01 +0300

    survey_card_created

---
+++ helloworld/app/src/main/java/com/example/helloworld/core/ui/survey_card/SurveyCard.kt
@@ -0,0 +1,54 @@
+package com.example.helloworld.core.ui.survey_card
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun SurveyCard(title: String, body: String) {
+    Surface(
+        modifier = Modifier
+            .padding(start = 20.dp, end = 20.dp, top = 20.dp)
+            .fillMaxWidth(),
+        shape = RoundedCornerShape(5.dp),
+        border = BorderStroke(1.dp, Color.Black)
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth(),
+            horizontalAlignment = Alignment.Start
+        ) {
+            Text(
+                text = title,
+                style = TextStyle(fontSize = 14.sp)
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(
+                text = body,
+                style = TextStyle(fontSize = 18.sp)
+            )
+        }
+    }
+}
+
+@Preview(showSystemUi = true, showBackground = true)
+@Composable
+fun SurveyCardPreview() {
+    LazyColumn {
+        items(5) { survey ->
+            SurveyCard(title = "Опрос от доктора $survey", body = "Как дела?")
+        }
+    }
+}

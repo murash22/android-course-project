0a6378eb542afd8e4993f0a11266708ad8762e55
10023r <ashyr22@icloud.com>
2024-03-23 15:28:18 +0300

    refactored

--- helloworld/app/src/main/java/com/example/helloworld/MainActivity.kt
+++ helloworld/app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -25,7 +25,7 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
 //                    MainScreen()
-                    PatientScreen()
+                    PatientScreen(patientId = "1")
                 }
             }
         }
--- helloworld/app/src/main/java/com/example/helloworld/Routes.kt
+++ helloworld/app/src/main/java/com/example/helloworld/Routes.kt
@@ -1,5 +1,6 @@
 package com.example.helloworld
 
+
 sealed class Routes(val route: String) {
     data object Home: Routes(route = "home")
     data object Other: Routes(route = "other")
@@ -12,6 +13,9 @@ sealed class PatientRoutes(
     data object TakeSurvey: PatientRoutes(route = "take_survey", argName = "id")
     fun takeSurveyWithArg(id: Int): String {
          return route + "/${id}"
+    }
+    fun takeSurveyWithArg(id: String): String {
+        return route + "/${id}"
     }
 }
 

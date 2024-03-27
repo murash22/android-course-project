72975adb83f88556394569be233050484a9d0fb4
Toropigeon <71664787+TheAnton1@users.noreply.github.com>
2024-03-27 16:03:38 +0300

    min-sdk-changes

--- patients/app/build.gradle.kts
+++ patients/app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "com.patients.main"
-        minSdk = 33
+        minSdk = 27
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
